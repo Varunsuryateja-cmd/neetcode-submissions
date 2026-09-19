1898ae89d1872337e644b50ac524cd5802917eff
Varun Surya Teja <varundangeti052@gmail.com>
2026-09-19 06:30:03 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+        public int longestConsecutive(int[] nums) {
+                java.util.HashSet<Integer> set = new java.util.HashSet<>();
+
+                        for (int num : nums) {
+                                    set.add(num);
+                                            }
+
+                                                    int longest = 0;
+
+                                                            for (int num : set) {
+                                                                        // Count only from the first number of a sequence
+                                                                                    if (!set.contains(num - 1)) {
+                                                                                                    int current = num;
+                                                                                                                    int length = 1;
+
+                                                                                                                                    while (set.contains(current + 1)) {
+                                                                                                                                                        current++;
+                                                                                                                                                                            length++;
+                                                                                                                                                                                            }
+
+                                                                                                                                                                                                            longest = Math.max(longest, length);
+                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                                        return longest;
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                            }
+
